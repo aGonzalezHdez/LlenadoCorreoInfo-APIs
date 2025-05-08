160cc0e6075debfa07544d0629b3ab939f819348
Torraskes <78329836+Torraskes@users.noreply.github.com>
2025-05-08 16:53:12 -0600

    feat: se agrega mapper y clase dto

---
+++ CRUDAPIs.Application/DTOs/ConsultasDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace CRUDAPIs.Application.DTOs;
+
+public class ConsultasDTO
+{
+    public int Id { get; set; }
+    public string FechaLlegada { get; set; }
+    public string HoraLlegada { get; set; }
+    public string CorreoCliente { get; set; }
+    public string TituloCorreo { get; set; }
+    public int Partidas { get; set; }
+    public string FechaCierre { get; set; }
+    public string HoraCierre { get; set; }
+    public int DiasRespuesta { get; set; }
+    public string Referencia { get; set; }
+    public string RazonSocial { get; set; }
+    public int ClienteId { get; set; } 
+    public int ComentariosId { get; set; }
+    public int AtendidoPorId { get; set; }
+   
+}
