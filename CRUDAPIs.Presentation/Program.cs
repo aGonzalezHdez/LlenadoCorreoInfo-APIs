d7cdb462eb75e6634db3a4710c2ca4fd2f4149a3
Torraskes <78329836+Torraskes@users.noreply.github.com>
2025-05-07 21:59:20 -0600

    Remove: Remover API inecesario e incluir Controladores

--- CRUDAPIs.Presentation/Program.cs
+++ CRUDAPIs.Presentation/Program.cs
@@ -11,6 +11,8 @@ public class Program
         // Add services to the container.
         builder.Services.AddAuthorization();
         
+        builder.Services.AddControllers();
+        
         // Add personal configurations 
         builder.Services.AddInfrastructureServices();
 
@@ -31,27 +33,9 @@ public class Program
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
-
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        {
-                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            TemperatureC = Random.Shared.Next(-20, 55),
-                            Summary = summaries[Random.Shared.Next(summaries.Length)]
-                        })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast")
-            .WithOpenApi();
-
+        
+        app.MapControllers();
+        
         app.Run();
     }
 }
